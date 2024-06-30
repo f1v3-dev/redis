823b8d2ad47898d982a7b0af495a8e86e8f57c82
seungjo <f1v3@kakao.com>
2024-06-30 16:14:36 +0900

    feat: spring session - redis로 관리

--- src/main/java/com/f1v3/redis/controller/RedisController.java
+++ src/main/java/com/f1v3/redis/controller/RedisController.java
@@ -3,7 +3,9 @@ package com.f1v3.redis.controller;
 import com.f1v3.redis.dto.RedisCreateRequest;
 import com.f1v3.redis.dto.RedisRequest;
 import com.f1v3.redis.service.RedisService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +24,10 @@ public class RedisController {
     @PostMapping("/setRedisStringValue")
     public void setRedisStringValue(@RequestBody RedisCreateRequest request) {
         redisService.setRedisStringValue(request.getKey(), request.getValue());
+    }
+
+    @GetMapping("/getSessionId")
+    public String getSessionId(HttpSession session) {
+        return session.getId();
     }
 }
